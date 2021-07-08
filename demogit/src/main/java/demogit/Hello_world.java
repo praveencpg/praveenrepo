bef063c959664462356fa8d15e890caa7e8a4ea9
praveencpg <praveengolu35@gmail.com>
2021-07-08 10:01:12 +0530

    java is added with hello

--- demogit/src/main/java/demogit/Hello_world.java
+++ demogit/src/main/java/demogit/Hello_world.java
@@ -3,8 +3,9 @@ package demogit;
 public class Hello_world {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello java world");
 
 	}
 
